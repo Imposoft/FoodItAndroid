a8a8144c9beecd27590de8364f599254ebe9bff5
leo-babogachi <almohanovidick.noemi@gmail.com>
2020-06-22 16:44:16 +0200

    modificacion de layouts

--- app/src/main/java/es/imposoft/foodit/model/Dish.java
+++ app/src/main/java/es/imposoft/foodit/model/Dish.java
@@ -6,17 +6,27 @@ import java.util.List;
 
 public class Dish {
 
+    int id;
     String name, description;
     List<Allergen> allergens;
     float price;
     Image image;
 
-    public Dish(String name, String description, List<Allergen> allergens, float price, Image image) {
+    public Dish(int id, String name, String description, List<Allergen> allergens, float price, Image image) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.allergens = allergens;
         this.price = price;
         this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
