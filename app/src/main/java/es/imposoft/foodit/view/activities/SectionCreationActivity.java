7e49a788dba1fff64ebcb2316a8da4b9b1429f1d
leo-babogachi <almohanovidick.noemi@gmail.com>
2020-06-22 14:21:57 +0200

    creados layouts
    creacion de menu, seccion y plato

---
+++ app/src/main/java/es/imposoft/foodit/view/activities/SectionCreationActivity.java
@@ -0,0 +1,16 @@
+package es.imposoft.foodit.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.imposoft.foodit.R;
+
+public class SectionCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_creation);
+    }
+}
